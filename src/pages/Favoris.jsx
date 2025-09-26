7ae04e2faddfce4b50f6901d6382aa7633b20e7f
khadija2378 <khadijaraja119@gmail.com>
2025-09-26 12:05:33 +0100

    Add navbar mobile

--- src/pages/Favoris.jsx
+++ src/pages/Favoris.jsx
@@ -1,6 +1,6 @@
 import HeroFavoris from "../components/HeroFavoris"
 import MainFavoris from "../components/MainFavoris"
-import Recherche from "../components/Recherche"
+
 
 function Favoris(){
     return(
